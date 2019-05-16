6a21a0e4388937ffc605c3b6b38274d2be07d279
Laptop Julien Maillet <julienmaillet2@gmail.com>
2019-05-16 14:10:04 +0200

    Add light bien

---
+++ Unity/Assets/Paul/Scripts/Scripts_Pour_Julo/ActivatingLight.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivatingLight : MonoBehaviour
+{
+
+    [Header("Toutes les lights dans l'ordre de spawn")]
+    public GameObject[] LightPrefab;
+    [Header("Le mesh des lights")]
+    public GameObject[] MeshLight;
+    [Header("L'emissive material pour les lights")]
+    public Material[] EmissiveLight;
+    [Space]
+    [Header("Lights Info")]
+    public float[] TempsEntreChaqueLight;
+    [Space]
+    [Header("Le Tag associé à ton player")]
+    public string PlayerTag;
+
+
+
+
+    private void Awake()
+    {
+        for (int i = 0, l = LightPrefab.Length; i < l; ++i)
+        {
+            if(LightPrefab[i] != null)
+            {
+                if (LightPrefab[i].activeSelf)
+                {
+                    LightPrefab[i].SetActive(false);
+                }
+            }
+        }
+    }
+
+
+    bool oneTime;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(tag) && !oneTime)
+        {
+            SpawnLights();
+            oneTime = true;
+        }
+    }
+
+
+
+
+    void SpawnLights()
+    {
+        StartCoroutine(SpawnLightWithTime());
+    }
+
+    IEnumerator SpawnLightWithTime()
+    {
+        for (int i = 0, l = LightPrefab.Length; i < l; ++i)
+        {
+            if (LightPrefab[i] != null)
+            {
+                yield return new WaitForSeconds(TempsEntreChaqueLight[i]);
+                LightPrefab[i].SetActive(true);
+                MeshLight[i].GetComponent<MeshRenderer>().material = EmissiveLight[i];
+
+            }
+        }
+    }
+
+}
