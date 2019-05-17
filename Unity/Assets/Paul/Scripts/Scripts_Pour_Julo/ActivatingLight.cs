f0d0377f5ae76e5520f58b13ffb65b76c86b4bea
Laptop Julien Maillet <julienmaillet2@gmail.com>
2019-05-17 16:00:46 +0200

    Add things

--- Unity/Assets/Paul/Scripts/Scripts_Pour_Julo/ActivatingLight.cs
+++ Unity/Assets/Paul/Scripts/Scripts_Pour_Julo/ActivatingLight.cs
@@ -66,6 +66,7 @@ public class ActivatingLight : MonoBehaviour
 
             }
         }
+        StopCoroutine(SpawnLightWithTime());
     }
 
 }
---
+++ Unity/Assets/Paul/Scripts/Scripts_Pour_Julo/TimedLight.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedLight : MonoBehaviour
+
+{
+
+    public GameObject _light;
+    [SerializeField] private bool actived = false;
+    [SerializeField] private float offTime = 0;
+    [SerializeField] private float timer = 5.0f;
+
+    private void Update()
+    {
+        if (!_light.activeSelf)
+        {
+            offTime += Time.deltaTime;
+
+            if (offTime > timer)
+            {
+                _light.SetActive(true);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player" && !actived)
+        {
+            _light.SetActive(false);
+            actived = true;
+        }
+    }
+}
