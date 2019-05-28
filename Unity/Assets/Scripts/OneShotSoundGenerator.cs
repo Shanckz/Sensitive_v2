0bdbeb106e0f5a1bae7456b6f5f92df286ea7733
Laptop_Jérémy <jeremy.payre@live.fr>
2019-05-28 18:12:41 +0200

    Add SenseManager.prefab & OneShotSoundGenerator.cs

--- Unity/Assets/Scripts/OneShotSoundGenerator.cs
+++ Unity/Assets/Scripts/OneShotSoundGenerator.cs
@@ -23,7 +23,6 @@ public class OneShotSoundGenerator : MonoBehaviour {
     {
         StartCoroutine(WaitForNewSound());
     }
-
     IEnumerator WaitForNewSound()
     {
         float i = NextSound();
@@ -32,30 +31,48 @@ public class OneShotSoundGenerator : MonoBehaviour {
         Debug.Log("NEW SOUND");
         StartSound();
     }
-
     private void StartSound()
     {
+        GameObject objToInstantiate = ChoseSound(ChoseSoundType());
+        Level.AddFX(objToInstantiate, Vector3.zero, Quaternion.identity);
 
         StartCoroutine(WaitForNewSound());
     }
 
+#region Return function
     private float NextSound()
     {
         return Random.Range(m_minTimer, m_maxTimer);
     }
-
     private int ChoseSoundType()
     {
         float f = Random.Range(0, 100);
+        Debug.Log("Random.Range(0, 100) = " + f);
         if(f < m_oneShotSounds[0].m_percentage)
         {
+            Debug.Log("return 0");
             return 0;
         }
-        else if (f < m_oneShotSounds[0].m_percentage)
+        else if (f > m_oneShotSounds[0].m_percentage && f < m_oneShotSounds[0].m_percentage + m_oneShotSounds[1].m_percentage)
         {
+            Debug.Log("return 1");
             return 1;
         }
+        else if (f > m_oneShotSounds[0].m_percentage + m_oneShotSounds[1].m_percentage)
+        {
+            Debug.Log("return 2");
+            return 2;
+        }
 
+        Debug.Log("return MERDE");
+        return 0;
     }
+    private GameObject ChoseSound(int soundArrayNb)
+    {
+        int alea = Random.Range(0, m_oneShotSounds[soundArrayNb].m_sounds.Length);
+        Debug.Log("ChoseSound alea = " + alea);
+        return m_oneShotSounds[soundArrayNb].m_sounds[alea];
+    }
+#endregion Return function
 
 }
