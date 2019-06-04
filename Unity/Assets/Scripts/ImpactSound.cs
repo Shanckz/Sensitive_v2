ba3b43b0cd3c615d8878f409c1d2589db8e90fe3
Laptop Julien Maillet <julienmaillet2@gmail.com>
2019-06-04 10:54:44 +0200

    add impact osund

---
+++ Unity/Assets/Scripts/ImpactSound.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImpactSound : MonoBehaviour {
+
+    public AudioSource impactSound;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.relativeVelocity.magnitude > 1)
+        {
+            impactSound.Play();
+        }
+    }
+}
