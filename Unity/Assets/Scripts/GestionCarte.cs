3f545e2b0c8248469f7d3a40a61a1e0a63d7d6dd
ValorisFR <valoris@hotmail.fr>
2019-06-13 23:53:14 +0200

    add scene GA + add script

---
+++ Unity/Assets/Scripts/GestionCarte.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GestionCarte : MonoBehaviour
+{
+    [SerializeField]
+    protected GameObject hs;
+    [SerializeField]
+    protected GameObject es;
+    [SerializeField]
+    protected GameObject open;
+
+    void Start ()
+    {
+        hs.SetActive(true);
+        es.SetActive(false);
+        open.SetActive(false);
+	}
+	
+	void Update ()
+    {
+		if(hs.activeSelf == true && openDoorLevier1.levierActived == true)
+        {
+            hs.SetActive(false);
+            es.SetActive(true);
+        }
+        if(es.activeSelf == true && openDoorKey1.bureauButtonActived == true)
+        {
+            es.SetActive(false);
+            open.SetActive(true);
+        }
+	}
+}
